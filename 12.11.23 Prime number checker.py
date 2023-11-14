8097aa9e08c69ff566a857e3fdc7ec8b202eb2ca
Alex Pivovarov <139866046+alexpivovarov@users.noreply.github.com>
2023-11-14 08:19:41 +0000

    Update 12.11.23 Prime number checker.py

--- 12.11.23 Prime number checker.py
+++ 12.11.23 Prime number checker.py
@@ -17,3 +17,24 @@ if number > 1:
     print(f"{number} is not prime")
 else:
   print(f"{number} is not prime")
+
+
+
+
+
+# using function
+
+
+def prime_checker(number):
+    is_prime = True
+    if number > 1:
+        for factor in range(2, number):
+            if number % factor == 0:
+                is_prime = False
+        if is_prime:
+            print("It's a prime number.")
+        else:
+            print("It's not a prime number.")
+
+n = int(input())  # Check this number
+prime_checker(number=n)
