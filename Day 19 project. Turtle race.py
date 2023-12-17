ae46d7de8c8a245e4fce19d84c0a9b0c26870986
Alex Pivovarov <139866046+alexpivovarov@users.noreply.github.com>
2023-12-17 12:38:55 +0000

    Create Day 19 project. Turtle race.py

---
+++ Day 19 project. Turtle race.py
@@ -0,0 +1,44 @@
+from turtle import Turtle, Screen
+import random
+
+is_race_on = False
+
+screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Type a color: ")
+print(user_bet)
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+y_positions = [-100, -50, 0, 50, 100, 150]
+all_turtles = []
+
+
+for turtle_index in range(0, 6):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.penup()
+    new_turtle.color(colors[turtle_index])
+    new_turtle.goto(x=-230, y=y_positions[turtle_index])
+    all_turtles.append(new_turtle)
+
+
+if user_bet:
+    is_race_on = True
+
+# Creating user_bet we prevent the loop from starting while user makes the bet
+
+while is_race_on:
+
+    for turtle in all_turtles:
+        if turtle.xcor() > 230: # End of the screen - x coord = 250. Turtle is 40x40 object => so centre of turtle
+            # will be at 250 - (40/2) = 230
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print("You have won!")
+            else:
+                print(f"Sorry, you lost. Winner is {winning_color}")
+
+        rand_distance = random.randint(0, 10)
+        turtle.forward(rand_distance)
+
+
+screen.exitonclick()
