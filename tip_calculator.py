bde9f3a7104e3fdf5af2c657b5d6f447c6a04a07
Alex Pivovarov <139866046+alexpivovarov@users.noreply.github.com>
2023-12-27 10:49:21 +0000

    Update tip_calculator.py

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,15 +1,35 @@
-#If the bill was $150.00, split between 5 people, with 12% tip. 
-
-#Each person should pay (150.00 / 5) * 1.12 = 33.6
-#Format the result to 2 decimal places = 33.60
-
-#Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
-
-#Write your code below this line 👇
-
 print('Welcome to the tip calculator')
-bill = float(input('What is the total bill? '))
-tips = float(input('What percentage tip would you like to give? (enter a value wihout the % sign) '))
-num_people =int(input('How many people to split the bill? '))
-num_each = "{:.2f}".format((bill + bill * (tips / 100)) / numb_people)  
-print(f"each person should pay {numb_each}")
+
+
+vaild_input = True
+
+while vaild_input == True:
+
+    bill = float(input('What is the total bill? '))
+    if bill < 0:
+        print("Bill amount cannot be negative.")
+        vaild_input= False
+        break
+
+    tip_percentage = float(input('What percentage tip would you like to give? (enter a value without the % sign) '))
+    if tip_percentage < 0:
+        print("Tip percentage cannot be negative")
+        vaild_input = False
+        break
+
+    num_people = int(input('How many people to split the bill with? '))
+    if num_people <= 0:
+        print('Number of people must be a positive integer.')
+        vaild_input = False
+        break
+
+    # Exit the loop if all inputs are valid
+    break
+
+if vaild_input:
+    total_bill_with_tip = bill + bill * (tip_percentage / 100)
+    amount_per_person = "{:.2f}".format(total_bill_with_tip / num_people)  # "{:.2f}".format(...)" is a string formatting
+    # method. The result will be formatted as a floating-point number with two digits after the decimal point.
+    print(f"each person should pay {amount_per_person}")
+else:
+    print("Invalid input provided. Exiting the tip calculator.")
