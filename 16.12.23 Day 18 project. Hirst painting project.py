052f3cea100a4ccd1ba242326354ad02647b9ccb
Alex Pivovarov <139866046+alexpivovarov@users.noreply.github.com>
2023-12-30 21:12:55 +0000

    Update 16.12.23 Day 18 project. Hirst painting project.py

--- 16.12.23 Day 18 project. Hirst painting project.py
+++ 16.12.23 Day 18 project. Hirst painting project.py
@@ -7,11 +7,13 @@ tim.penup()
 tim.hideturtle()
 
 
+# list containing tuples of different colours
 color_list = [(170, 79, 56), (191, 152, 112), (133, 32, 61), (127, 63, 78), (73, 98, 115), (217, 91, 50), (125, 168, 149),
               (94, 120, 108), (58, 48, 92), (47, 31, 82), (71, 28, 76), (60, 167, 177), (86, 155, 143), (211, 204, 154),
               (91, 186, 196), (142, 133, 102), (110, 218, 224), (153, 34, 26), (137, 215, 197), (172, 93, 100), (236, 235, 240),
               (159, 135, 140), (66, 44, 41), (37, 73, 84), (124, 126, 140)]
 
+# Making turtle white so that it is invisible on a white background
 turtle_module.colormode(255)
 
 # Making turtle start at the left down corner of a screen
@@ -21,6 +23,7 @@ tim.setheading(0)
 number_of_dots = 100
 
 
+# Making a line of dots
 for dot_count in range(1, number_of_dots + 1):
     tim.dot(20, random.choice(color_list))
     tim.forward(50)
