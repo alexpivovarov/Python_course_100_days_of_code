ceacf9f682141198ec2840caa7f61c8ced727715
Alex Pivovarov <139866046+alexpivovarov@users.noreply.github.com>
2023-12-30 12:19:07 +0000

    Update 18.11.23 ceaser cypher.py

--- 18.11.23 ceaser cypher.py
+++ 18.11.23 ceaser cypher.py
@@ -1,39 +1,27 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 def caesar(start_text, shift_amount, cipher_direction):
-  end_text = ""
-  if cipher_direction == "decode":
-    shift_amount *= -1
-
-# if user enter character different from a letter then it will remain unchanged
-  for char in start_text:
-    if char in alphabet:
-      position = alphabet.index(char)
-      new_position = position + shift_amount
-      end_text += alphabet[new_position]
-    else:
-      end_text += char
-   
-    
-    
-  print(f"The {cipher_direction}d result is: {end_text}")
-
-
+    end_text = ""
+    if cipher_direction == "decode":
+        shift_amount *= -1
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = (position + shift_amount) % len(alphabet) # calculates the new position and uses % len(alphabet) to wrap around if the end of the alphabet is reached
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
+    print(f"The {cipher_direction}d result is: {end_text}")
 
 should_continue = True
 while should_continue:
-  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-  text = input("Type your message:\n").lower()
-  shift = int(input("Type the shift number:\n"))
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n")) % 26 # "% 26" is to ensure that the shift value is always within the range of 0 to 25 (the number of letters in the English alphabet).
 
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
 
-
-# the line below enables to execute code when shift number is bigger than 26 + 26 = 52
-  shift = shift % 26
-
-  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
-
-  restart = input("Enter 'yes' if you want to restart, 'no' for exit.\n")
-  if restart == "no":
-    should_continue = False
-    print("Goodbye")
+    restart = input("Type 'yes' to restart, 'no' to exit:\n").lower()
+    if restart == "no":
+        should_continue = False
+        print("Goodbye")
