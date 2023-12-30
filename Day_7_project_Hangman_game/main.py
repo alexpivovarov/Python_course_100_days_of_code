1287d255c9866fca1ef4107d4f65c8cb5c3df647
Alex Pivovarov <139866046+alexpivovarov@users.noreply.github.com>
2023-12-30 11:46:25 +0000

    Update main.py

--- Day_7_project_Hangman_game/main.py
+++ Day_7_project_Hangman_game/main.py
@@ -18,32 +18,31 @@ display = []
 for _ in range(word_length):
     display += "_"
 
+guessed_letters = []  # Keep track of guessed letters
+
+
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
 
     # If the user has entered a letter they've already guessed
-    if guess in display:
+    if guess in guessed_letters:
         print(f"You've already guessed {guess}")
+    else:
+        guessed_letters.append(guess)  # Add new guesses to the list
+        if guess in chosen_word:
+            for position in range(word_length):
+                letter = chosen_word[position]
+                if letter == guess:
+                    display[position] = letter
+            print("Correct!")
+        else:
+            print(f"You guessed {guess}, that's not in the word. You have lost a life.")
+            lives -= 1
+            if lives == 0:
+                end_of_game = True
+                print("You have lost.")
+                print(f"Correct word was {chosen_word}.")
 
-    # Checking guessed letter
-    for position in range(word_length):
-        letter = chosen_word[position]
-
-        
-        if letter == guess:
-          display[position] = letter
-                print("Correct!")
-
-
-    #Checking if user is wrong.
-    if guess not in chosen_word:
-        print(f"You guessed {guess}, that's not in the word. You have lost a life.")
-        
-        lives -= 1
-        if lives == 0:
-            end_of_game = True
-            print("You have lost.")
-            print(f"Correct word was {chosen_word}.")
 
     #Joining all the elements in the list and turning it into a string.
     print(f"{' '.join(display)}")
